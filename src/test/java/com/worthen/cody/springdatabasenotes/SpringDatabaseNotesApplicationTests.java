bf25baa662f6c5ce857b02c83a12eb998c5fc05f
Cody Worthen <cworthen777@hotmail.com>
2022-05-07 11:13:11 -0700

    first commit

---
+++ src/test/java/com/worthen/cody/springdatabasenotes/SpringDatabaseNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worthen.cody.springdatabasenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatabaseNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
